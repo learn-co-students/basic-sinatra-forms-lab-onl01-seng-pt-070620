aaa2c8dde8e489d314f90b9dbfb590e6793bb506
Cristian Deleon <libaration@gmail.com>
2020-09-11 06:53:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'sinatra/base'
+require 'sanitize'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @center = params[:c]
+    erb :team
+  end
 
 end
