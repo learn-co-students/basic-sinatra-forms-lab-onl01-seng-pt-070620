1576919aada04e75fd60bc07fdafb0002a4a2a13
Raveen K Sharma <raveen08@gmail.com>
2020-09-28 21:23:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+  get '/newteam' do
+    erb :newteam
+  end
+  
+  post '/team' do
+   @Name = params[:name]
+   @Coach = params[:coach]
+   @PointGuard = params[:pg]
+   @ShootingGuard = params[:sg]
+   @SmallForward = params[:sf]
+   @PowerForward = params[:pf]
+   @Center = params[:c]
+   erb :team
+  end
 
 end
