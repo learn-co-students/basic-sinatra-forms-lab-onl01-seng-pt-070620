64c537ffb37f95dc7062af92aed5e6de08bb2954
Keesha Hargrove <kharg14@gmail.com>
2020-10-03 15:46:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,26 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post '/team' do
+        erb :team
+    end
 
 end
+
+
+
+
+
+
+# require 'sinatra/base'
+
+# class App < Sinatra::Base
+
+
+# end
